22ae890f0e6ceb80333e4975abda64ef24bb7c82
mistereye <yozgatolga66@gmail.com>
2019-12-17 20:03:08 +0100

    init

---
+++ src/main/java/fr/idmc/miage/apicredit/ApicreditApplication.java
@@ -0,0 +1,13 @@
+package fr.idmc.miage.apicredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicreditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/idmc/miage/apicredit/ApicreditApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.idmc.miage.apicredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
